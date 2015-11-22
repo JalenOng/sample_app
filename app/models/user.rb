731cc190c24685102f7ec545b0df715bd2e1d165
Jalen <jalen.ong@gmail.com>
2015-11-22 09:45:45 +0800

    Feed and following

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: {minimum: 6}, allow_blank: true
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+			  foreign_key: "followed_id", dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 
 	attr_accessor :remember_token
 
@@ -25,8 +30,25 @@ class User < ActiveRecord::Base
 	end
 
 	def feed
-		Micropost.where("user_id = ?", id)
+		following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+		Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
 
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 
